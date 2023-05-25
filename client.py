46d996cc012bb8fa3ee1e0a8d153da6e92798916
Ahmed Bakry <bbakry20@gmail.com>
2023-05-25 06:09:37 +0300

    Added Login Screen and resolved conflicts

--- client.py
+++ client.py
@@ -7,6 +7,183 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
+from email.header import decode_header
+import imaplib
+import email
+import socket
+import hashlib
+import re
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+def decrypt_string(key, ciphertext):
+    key = key.encode()
+    key = key[:16]
+    backend = default_backend()
+
+    # Static Initialization Vector (IV)
+    iv = b'ThisIsAStaticIV.'  # 16-byte IV
+
+    # Create the cipher object
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    decryptor = cipher.decryptor()
+
+    # Decrypt the ciphertext
+    decrypted_data = decryptor.update(bytes.fromhex(ciphertext)) + decryptor.finalize()
+
+    # Remove the padding from the decrypted plaintext
+    unpadder = padding.PKCS7(128).unpadder()
+    plaintext = unpadder.update(decrypted_data) + unpadder.finalize()
+
+    # Return the decrypted plaintext
+    return plaintext.decode()
+
+def encrypt_string(hash_string):
+    sha_signature = \
+        hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
+def extract_keys(string):
+    pattern = r'(\S+)\s*,\s*(\S+)'
+    match = re.match(pattern, string)
+    if match:
+        return match.groups()
+    else:
+        return None
+
+def get_master_key(username,password):
+    host = "127.0.0.1"  # Server's IP address
+    port = 10000        # Server's port number
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect to the server
+    client_socket.connect((host, port))
+    encryptedpass = encrypt_string(password)
+    # Send a message to the server
+    message = username + "\n" + encryptedpass
+    client_socket.send(message.encode())
+    # Receive and print the server's response
+    data = client_socket.recv(2048)
+    client_socket.close()
+    masterkey = (decrypt_string(encryptedpass,data.decode()))
+    print(masterkey)
+    return masterkey
+def getsessionkeys(user1,user2):
+    host = "127.0.0.1"  # Server's IP address
+    port = 10000        # Server's port number
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect to the server
+    client_socket.connect((host, port))
+    message = user1 + "," + user2
+    client_socket.send(message.encode())
+    data = client_socket.recv(2048)
+    client_socket.close()
+    sessionkeys = extract_keys(data.decode())
+    print(sessionkeys) # you have decrypt this using the masterkey bakry
+    return sessionkeys
+
+#get_master_key("username" , "password")
+#getsessionkeys("username 1" , "username 2")
+
+
+class Login_Page:
+
+    def __init__(self, login):  # This is my first change so i already initialize a Tk window inside the class
+        """
+        :type login: object
+        """
+        self.login = login
+        login.protocol("WM_DELETE_WINDOW",self.event_X)
+        login.title("Login")
+        login.geometry("450x230+450+170")
+
+    # Creating describtioneves
+
+        self.username = Label(login, text="Email:")
+        self.username.place(relx=0.285, rely=0.298, height=20, width=55)
+
+        self.password = Label(login, text="Password:")
+        self.password.place(relx=0.285, rely=0.468, height=20, width=55)
+
+        # Creating Buttons
+
+        self.login_button = Button(login, text="Login")
+        self.login_button.place(relx=0.440, rely=0.638, height=30, width=60)
+        self.login_button.configure(command=self.login_user)
+
+        self.login_completed = IntVar()
+
+        self.exit_button = Button(login, text="Exit")  # , command=master.quit)
+        self.exit_button.place(relx=0.614, rely=0.638, height=30, width=60)
+        self.exit_button.configure(command=self.exit_login)
+
+        # Creating entry boxes
+
+        self.username_box = Entry(login)
+        self.username_box.place(relx=0.440, rely=0.298, height=20, relwidth=0.35)
+
+        self.password_box = Entry(login)
+        self.password_box.place(relx=0.440, rely=0.468, height=20, relwidth=0.35)
+        self.password_box.configure(show="*")
+        self.password_box.configure(background="white")
+
+        # Creating checkbox
+
+        self.var = IntVar()
+        self.show_password = Checkbutton(login)
+        self.show_password.place(relx=0.285, rely=0.650, relheight=0.100, relwidth=0.125)
+        self.show_password.configure(justify='left')
+        self.show_password.configure(text='''Show''')
+        self.show_password.configure(variable=self.var, command=self.cb)
+
+    def event_X(self):
+        if messagebox.askokcancel("Exit", "Are you sure you want to exit?"):
+            exit()
+
+    def cb(self, ):
+        if self.var.get() == True:
+            self.password_box.configure(show="")
+        else:
+            self.password_box.configure(show="*")
+
+
+# Giving function to login process
+
+    def login_user(self):
+        name = self.username_box.get()
+        passw = self.password_box.get()
+        login_completed = self.login_completed.get()
+
+        if name and passw:
+            # messagebox.showinfo("Login page", "Login successful!")
+            self.login.destroy()  # Removes the toplevel window
+            # self.main_win.deiconify() #Unhides the root window
+            self.login_completed == 1
+
+            global sender
+            global password
+
+            sender = name
+            password = passw
+
+        else:
+            messagebox.showwarning("Login Failed - Acess Denied", "Username or Password incorrect!")
+
+        # return
+
+
+    def exit_login(self):
+        msg = messagebox.askyesno("Exit login page", "Do you really want to exit?")
+        if (msg):
+            exit()
+
+
+    def mainloop_window(self):  # This is the class function that helps me to mainloop the window
+        self.login.mainloop()
+
+
 class App:
     tovar=""
 
@@ -239,8 +416,8 @@ class App:
 
 
 if __name__ == "__main__":
-    # login_page = Login_Page(tk.Tk())  # I dont need to pass the root now since its initialized inside the class
-    # login_page.mainloop_window()  # Just mainlooping the authentication window
+    login_page = Login_Page(tk.Tk())  # I dont need to pass the root now since its initialized inside the class
+    login_page.mainloop_window()  # Just mainlooping the authentication window
 
     root = tk.Tk()
     app = App(root)
